2b9a2aff61048061456a61070849549db4ad2f7e
pannilla <camilla.panni@gmail.com>
2020-04-08 15:10:23 +0200

    4 files modified, 5 added, 1 deleted

---
+++ src/app/supermarket.ts
@@ -0,0 +1,4 @@
+export interface SuperMarket {
+  id: number;
+  name: string;
+}
---
+++ src/app/supermarkets_list.ts
@@ -0,0 +1,9 @@
+import {SuperMarket} from './supermarket'
+
+export const SUPERMARKETS: SuperMarket [] = [
+  { id: 1, name: 'Bennet' },
+  { id: 2, name: 'Esselunga' },
+  { id: 3, name: 'Esselunga drive' },
+  { id: 4, name: 'Coop' },
+  { id: 5, name: 'SuperMercato24' }
+];
